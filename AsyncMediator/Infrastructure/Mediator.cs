8377cda35b87becb68575df404596120ef9d70d8
Jon van Doore <jpv001@gmail.com>
2016-03-29 16:46:38 +1100

    Added handler caching for Queries and LoadLists, refactored Event processor to guard against null events, rearranged declarations

--- AsyncMediator/Infrastructure/Mediator.cs
+++ AsyncMediator/Infrastructure/Mediator.cs
@@ -13,15 +13,14 @@ namespace AsyncMediator
         private readonly SingleInstanceFactory _singleInstanceFactory;
 
         private readonly ConcurrentBag<Func<Task>> _deferredEvents = new ConcurrentBag<Func<Task>>();
-
         //This will allow for the caching of handler types so that they're not constantly created
-        private readonly ConcurrentDictionary<Type, Type> _handlerCache;
+        private readonly ConcurrentDictionary<Type, Type> _handlerCache = new ConcurrentDictionary<Type, Type>();
+        
 
         public Mediator(MultiInstanceFactory multiInstanceFactory, SingleInstanceFactory singleInstanceFactory)
         {
             _multiInstanceFactory = multiInstanceFactory;
             _singleInstanceFactory = singleInstanceFactory;
-            _handlerCache = new ConcurrentDictionary<Type, Type>();
         }
 
         private async Task Publish<TEvent>(TEvent @event) where TEvent : IDomainEvent
@@ -44,25 +43,24 @@ namespace AsyncMediator
 
         public async Task ExecuteDeferredEvents()
         {
-            while (_deferredEvents.Any())
+            Func<Task> @event;
+            while (_deferredEvents.TryTake(out @event))
             {
-                Func<Task> @event;
-                _deferredEvents.TryTake(out @event);
                 await @event.Invoke().ConfigureAwait(false);
             }
         }
 
         public async Task<TResult> LoadList<TResult>()
         {
-            var queryHandler = typeof(ILookupQuery<>).MakeGenericType(typeof(TResult));
-            var handler = (ILookupQuery<TResult>)_singleInstanceFactory(queryHandler);
+            var genericHandlerType = _handlerCache.GetOrAdd(typeof(TResult), typeof(ILookupQuery<>).MakeGenericType(typeof(TResult)));
+            var handler = (ILookupQuery<TResult>)_singleInstanceFactory(genericHandlerType);
             return await handler.Query();
         }
 
         public async Task<TResult> Query<TCriteria, TResult>(TCriteria criteria)
         {
-            var queryHandler = typeof(IQuery<,>).MakeGenericType(typeof(TCriteria), typeof(TResult));
-            var handler = (IQuery<TCriteria, TResult>)_singleInstanceFactory(queryHandler);
+            var genericHandlerType = _handlerCache.GetOrAdd(typeof(IQuery<TCriteria, TResult>), typeof(IQuery<,>).MakeGenericType(typeof(TCriteria), typeof(TResult)));
+            var handler = (IQuery<TCriteria, TResult>)_singleInstanceFactory(genericHandlerType);
             return await handler.Query(criteria);
         }
 
